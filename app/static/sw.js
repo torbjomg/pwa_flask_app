058d9a8186d3ee49ab4aa2e3c7166f18a12a6f52
torbjomg <torbjorngul@gmail.com>
2020-04-13 17:53:14 +0200

    add pwa back in, test indexedDb

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,6 +1,6 @@
 console.log('Hello from sw.js');
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
