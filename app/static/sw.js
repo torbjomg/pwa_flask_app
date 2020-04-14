d1dfbb4ac7c07855f68415ecf3237a6ed5117779
torbjomg <torbjorngul@gmail.com>
2020-04-14 14:26:12 +0200

    removed unused font chace

--- app/static/sw.js
+++ app/static/sw.js
@@ -31,18 +31,6 @@ if (workbox) {
       ],
     }),
   );
-
-  workbox.routing.registerRoute(
-    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'googleapis',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 30,
-        }),
-      ],
-    }),
-  );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
